8a3cf7f43f6c57f1a60100e7f3112584e3d73bb4
jhonh10 <jhonhpalacios09@gmail.com>
2022-10-31 10:02:27 -0500

    mod default query client

--- src/App.js
+++ src/App.js
@@ -2,14 +2,7 @@ import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import Router from './routes';
 
 export default function App() {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-        cacheTime: Infinity
-      }
-    }
-  });
+  const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <Router />
