785bf0ca20cefe0c9fad379c9e909e8ca6ec5b33
jhonh10 <jhonhpalacios09@gmail.com>
2022-11-08 18:16:40 -0500

    receive data by parameter when the data is load

---
+++ src/pages/allStudents.js
@@ -0,0 +1,28 @@
+import { Page } from '@shopify/polaris';
+import { useNavigate } from 'react-router-dom';
+import ListTable from '../components/ListTable';
+
+export const AllStudents = ({ students }) => {
+  const navigate = useNavigate();
+  const handlestudentCreate = () => navigate('new');
+
+  return (
+    <Page
+      title="Alumnos"
+      fullWidth
+      primaryAction={{
+        content: 'Agregar Alumno',
+        onAction: handlestudentCreate
+      }}
+      secondaryActions={[
+        {
+          content: 'Importar',
+          disabled: false,
+          helpText: 'You need permission to import products.'
+        }
+      ]}
+    >
+      <ListTable students={students} />
+    </Page>
+  );
+};
