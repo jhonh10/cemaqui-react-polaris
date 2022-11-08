5f7257592de35b9d413dbe0f0445efaba533de84
jhonh10 <jhonhpalacios09@gmail.com>
2022-11-08 18:11:18 -0500

    create delete function

--- src/firebase/client.js
+++ src/firebase/client.js
@@ -7,7 +7,9 @@ import {
   doc,
   setDoc,
   Timestamp,
-  getDoc
+  getDoc,
+  deleteDoc,
+  updateDoc
 } from 'firebase/firestore';
 
 const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
@@ -33,7 +35,10 @@ export async function getStudents() {
   const studentsColl = collection(db, 'Alumnos');
   const studentSnapShot = await getDocs(studentsColl);
   const studentList = studentSnapShot.docs.map(mapStudentFromFirebase);
-  return studentList;
+  if (studentList.length > 0) {
+    return studentList;
+  }
+  return null;
 }
 
 export async function getStudentById(studentId) {
@@ -44,12 +49,25 @@ export async function getStudentById(studentId) {
     const student = mapStudentFromFirebase(docSnap);
     return student;
   }
-  return console.log('No data');
+  return null;
 }
 
 export async function addStudent(values) {
   await setDoc(doc(db, 'Alumnos', `${values.documentId}`), {
     ...values,
     expeditionDate: Timestamp.fromDate(new Date())
+  });
+}
+
+export async function deleteStudent(id, loading = () => {}) {
+  loading(true);
+  await deleteDoc(doc(db, 'Alumnos', id));
+  loading(false);
+}
+
+export async function updateStudentData(id, data) {
+  const studentDocRef = doc(db, 'Alumnos', id);
+  await updateDoc(studentDocRef, {
+    notes: data
   });
 }
