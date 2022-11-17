4b3b5c4c9a03dd479681d9244e477ca4305fa1d2
jhonh10 <jhonhpalacios09@gmail.com>
2022-11-16 19:39:09 -0500

    create card component for list student contactInfo

--- src/components/studentDetails/ContactInfoCard.js
+++ src/components/studentDetails/ContactInfoCard.js
@@ -1,7 +1,17 @@
-import { Button, Card, Stack, Subheading, Text, TextContainer, TextStyle } from '@shopify/polaris';
-import React from 'react';
+import { useCallback, useState } from 'react';
+import { useOutletContext } from 'react-router-dom';
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
+import { Button, Card, Subheading, TextContainer, Stack, Text, TextStyle } from '@shopify/polaris';
+import { ModalForm } from '../ModalForm';
+import { updateStudentData } from '../../firebase/client';
+import { ContactInfoForm } from './ContacInfoForm';
 
-export const ContactInfoCard = ({ email, phone }) => {
+export const ContactInfoCard = ({ email, phone, id, refetch }) => {
+  const [openModal, setOpenModal] = useState(false);
+  const toggleOpenModal = useCallback(() => setOpenModal((openModal) => !openModal), []);
+  const { handleToast } = useOutletContext();
+
   const studentEmail = email ? (
     <Text>{email}</Text>
   ) : (
@@ -13,26 +23,80 @@ export const ContactInfoCard = ({ email, phone }) => {
   ) : (
     <TextStyle variation="subdued">No se ingreso un numero de telefono</TextStyle>
   );
+
+  const initialValues = {
+    phone,
+    email
+  };
+
+  const registerSchema = Yup.object().shape({
+    phone: Yup.string()
+      .matches(/^[0-9]+$/, 'Ingrese un numero de telefono valido')
+      .min(10, 'No parece un numero valido'),
+    email: Yup.string().email('No parece un email valido')
+  });
+
+  const onSubmit = async () => {
+    try {
+      await updateStudentData({ docId: id, data: { email: values.email, phone: values.phone } });
+      await refetch();
+      setOpenModal(false);
+      handleToast('Los datos del alumno han sido actualizados');
+    } catch (error) {
+      handleToast(error);
+    }
+  };
+  const formik = useFormik({
+    initialValues,
+    validationSchema: registerSchema,
+    enableReinitialize: true,
+    onSubmit
+  });
+
+  const { errors, values, isSubmitting, dirty, handleSubmit, setFieldValue, handleReset } = formik;
+
+  const cancelAction = () => {
+    toggleOpenModal();
+    handleReset();
+  };
+
+  const modalForm = (
+    <ModalForm
+      open={openModal}
+      title="Editar informacion de contacto"
+      cancelAction={cancelAction}
+      disabled={!dirty}
+      body={<ContactInfoForm values={values} setFieldValue={setFieldValue} error={errors} />}
+      confirmAction={handleSubmit}
+      loading={isSubmitting}
+    />
+  );
+
   return (
-    <Card.Section>
-      <Stack vertical>
-        <Stack>
-          <Stack.Item fill>
-            <Subheading>Informacion de contácto</Subheading>
-          </Stack.Item>
-          <Button plain>Editar</Button>
-        </Stack>
+    <>
+      {modalForm}
+      <Card.Section>
         <Stack vertical>
-          <TextContainer spacing="tight">
-            <Stack vertical>
-              <Stack vertical spacing="tight">
-                {studentEmail}
-                {studentPhone}
+          <Stack>
+            <Stack.Item fill>
+              <Subheading>Informacion de contácto</Subheading>
+            </Stack.Item>
+            <Button plain onClick={toggleOpenModal}>
+              Editar
+            </Button>
+          </Stack>
+          <Stack vertical>
+            <TextContainer spacing="tight">
+              <Stack vertical>
+                <Stack vertical spacing="tight">
+                  {studentEmail}
+                  {studentPhone}
+                </Stack>
               </Stack>
-            </Stack>
-          </TextContainer>
+            </TextContainer>
+          </Stack>
         </Stack>
-      </Stack>
-    </Card.Section>
+      </Card.Section>
+    </>
   );
 };
