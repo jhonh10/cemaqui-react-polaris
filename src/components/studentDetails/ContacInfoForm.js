a1bbc71c3b1862f27abbe9bc794d4976b7d7eec8
jhonh10 <jhonhpalacios09@gmail.com>
2022-11-16 19:38:39 -0500

    create form for update student contact info

---
+++ src/components/studentDetails/ContacInfoForm.js
@@ -0,0 +1,25 @@
+import { Form, FormikProvider } from 'formik';
+import { TextField, Stack } from '@shopify/polaris';
+
+export const ContactInfoForm = ({ values, setFieldValue, error }) => (
+  <FormikProvider value={values}>
+    <Form autoComplete="off" noValidate>
+      <Stack vertical>
+        <TextField
+          placeholder="Email"
+          value={values.email}
+          onChange={(value) => setFieldValue('email', value)}
+          autoComplete="off"
+          error={error.email}
+        />
+        <TextField
+          placeholder="Numero de telefono"
+          value={values.phone}
+          onChange={(value) => setFieldValue('phone', value)}
+          autoComplete="off"
+          error={error.phone}
+        />
+      </Stack>
+    </Form>
+  </FormikProvider>
+);
