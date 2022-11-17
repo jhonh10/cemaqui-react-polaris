b55297b38b60fc397a1783e37e82813a54638c0e
jhonh10 <jhonhpalacios09@gmail.com>
2022-11-16 19:38:04 -0500

    create card component for list student address

--- src/components/studentDetails/AdressInfoCard.js
+++ src/components/studentDetails/AdressInfoCard.js
@@ -1,25 +1,91 @@
-import { Button, Card, Stack, Subheading, Text, TextContainer } from '@shopify/polaris';
-import React from 'react';
+import { Button, Card, Stack, Subheading, Text, TextContainer, TextStyle } from '@shopify/polaris';
+import React, { useCallback, useState } from 'react';
+import { useOutletContext } from 'react-router-dom';
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
+import { AddressInfoForm } from './AddressInfoForm';
+import { updateStudentData } from '../../firebase/client';
+import { ModalForm } from '../ModalForm';
 
-export const AdressInfoCard = ({ adress }) => (
-  <Card.Section>
-    <Stack vertical>
-      <Stack>
-        <Stack.Item fill>
-          <Subheading>Dirección predeterminada</Subheading>
-        </Stack.Item>
-        <Button plain>Gestionar</Button>
-      </Stack>
-      <Stack vertical>
-        <TextContainer spacing="tight">
-          <Stack vertical>
-            <Stack vertical spacing="tight">
-              <Text>Calle 8a 11-55</Text>
-              <Text>Bogota, Colombia</Text>
-            </Stack>
+export const AdressInfoCard = ({ address, id, refetch }) => {
+  const [openModal, setOpenModal] = useState(false);
+  const toggleOpenModal = useCallback(() => setOpenModal((openModal) => !openModal), []);
+  const { handleToast } = useOutletContext();
+
+  const studentAddress = address ? (
+    <Text>{address}</Text>
+  ) : (
+    <TextStyle variation="subdued">No se ingreso una dirección</TextStyle>
+  );
+
+  const initialValues = {
+    address
+  };
+
+  const registerSchema = Yup.object().shape({
+    address: Yup.string()
+  });
+
+  const onSubmit = async () => {
+    try {
+      await updateStudentData({ docId: id, data: { address: values.address } });
+      await refetch();
+      setOpenModal(false);
+      handleToast('Los datos del alumno han sido actualizados');
+    } catch (error) {
+      handleToast(error);
+    }
+  };
+  const formik = useFormik({
+    initialValues,
+    validationSchema: registerSchema,
+    enableReinitialize: true,
+    onSubmit
+  });
+
+  const { errors, values, isSubmitting, dirty, handleSubmit, setFieldValue, handleReset } = formik;
+
+  const cancelAction = () => {
+    toggleOpenModal();
+    handleReset();
+  };
+
+  const modalForm = (
+    <ModalForm
+      open={openModal}
+      title="Editar dirección"
+      cancelAction={cancelAction}
+      disabled={!dirty}
+      body={<AddressInfoForm values={values} setFieldValue={setFieldValue} error={errors} />}
+      confirmAction={handleSubmit}
+      loading={isSubmitting}
+    />
+  );
+  return (
+    <>
+      {modalForm}
+      <Card.Section>
+        <Stack vertical>
+          <Stack>
+            <Stack.Item fill>
+              <Subheading>Dirección predeterminada</Subheading>
+            </Stack.Item>
+            <Button plain onClick={toggleOpenModal}>
+              Gestionar
+            </Button>
           </Stack>
-        </TextContainer>
-      </Stack>
-    </Stack>
-  </Card.Section>
-);
+          <Stack vertical>
+            <TextContainer spacing="tight">
+              <Stack vertical>
+                <Stack vertical spacing="tight">
+                  {studentAddress}
+                  <Text>Bogota, Colombia</Text>
+                </Stack>
+              </Stack>
+            </TextContainer>
+          </Stack>
+        </Stack>
+      </Card.Section>
+    </>
+  );
+};
