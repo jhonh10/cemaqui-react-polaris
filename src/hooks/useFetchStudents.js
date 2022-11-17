b163496f97cd94a3c826fb4838945344a84b0dfd
jhonh10 <jhonhpalacios09@gmail.com>
2022-11-16 19:43:57 -0500

    add async function

--- src/hooks/useFetchStudents.js
+++ src/hooks/useFetchStudents.js
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { getStudents } from '../firebase/client';
 
 export const useFetchStudents = () => {
-  const { data, isLoading } = useQuery(['student'], getStudents, {
+  const { data, isLoading } = useQuery(['student'], async () => getStudents(), {
     cacheTime: Infinity,
     refetchOnWindowFocus: false
   });
