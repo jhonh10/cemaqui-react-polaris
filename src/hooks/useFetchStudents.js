48e3f79eadea6561c87eaac562720104bbf27030
Jhon Palacios <jhonhpalacios09@gmail.com>
2024-05-19 14:13:38 -0500

    mod fetch por pagination

--- src/hooks/useFetchStudents.js
+++ src/hooks/useFetchStudents.js
@@ -1,11 +1,28 @@
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getStudents } from "../firebase/client";
 
 export const useFetchStudents = () => {
-  const { data, isLoading, isError, error } = useQuery({
-    queryKey: ["students"],
-    queryFn: getStudents,
+  const [page, setPage] = useState(1);
+  const [pageAction, setPageAction] = useState(null);
+  const [firstVisible, setFirstVisible] = useState(null);
+  const [lastVisible, setLastVisible] = useState(null);
+
+  console.log(pageAction, lastVisible, firstVisible);
+  const { data, isLoading, isError, error, isPreviousData } = useQuery({
+    queryKey: ["students", page],
+    queryFn: () => getStudents(page, lastVisible, setLastVisible),
     refetchOnWindowFocus: false,
+    keepPreviousData: true,
   });
-  return { students: data, isLoading, isError, error };
+  return {
+    data,
+    isLoading,
+    isError,
+    error,
+    isPreviousData,
+    page,
+    setPage,
+    setPageAction,
+  };
 };
