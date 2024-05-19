d5191a56ff943b3b337f66820dff9132b51fd3e6
Jhon Palacios <jhonhpalacios09@gmail.com>
2024-05-19 14:14:35 -0500

    add variables for pagination

--- src/pages/allStudentsFlag.js
+++ src/pages/allStudentsFlag.js
@@ -2,10 +2,27 @@ import { useFetchStudents } from "../hooks/useFetchStudents";
 import { AllStudents } from "./allStudents";
 
 export default function AllStudentsFlag() {
-  const { students, isLoading, isError, error } = useFetchStudents();
-
+  const {
+    data,
+    isLoading,
+    isError,
+    error,
+    isPreviousData,
+    page,
+    setPage,
+    setPageAction,
+  } = useFetchStudents();
   if (isLoading) return <div>Cargando...</div>;
   if (isError) return <div>´se ha producido un error ${error}´</div>;
-  if (!students) return <div>No students</div>;
-  return <AllStudents students={students.studentList} />;
+  if (!data.studentList) return <div>No students</div>;
+  console.log(data.studentList);
+  return (
+    <AllStudents
+      students={data?.studentList}
+      setPage={setPage}
+      page={page}
+      hasMore={data?.hasMore}
+      setPageAction={setPageAction}
+    />
+  );
 }
